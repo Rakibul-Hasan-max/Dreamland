b59c6fb700daacde4c9149e6e08d10706c3b10c6
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-10 17:16:35 +0600

    footer

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <Typography variant="body2" style={{backgroundColor: "#19334A", color: "white", padding: "20px", marginBottom: "0" }} gutterBottom>
+        Copyright ©2021 Dreamland. All rights reserved.
+      </Typography>
+    );
+};
+
+export default Footer;
