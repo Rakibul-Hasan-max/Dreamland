a3e5536fc5d22b6b5f39328dd7a47e7e918e11ce
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-11 00:40:12 +0600

    lochva

--- src/App.js
+++ src/App.js
@@ -4,29 +4,32 @@ import Home from './Pages/Home/Home/Home';
 import NotFound from './Pages/NotFound/NotFound';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
+import AuthProvider from './contexts/AuthProvider/AuthProvider';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route path="/home">
-            <Home></Home>
-          </Route>
-          <Route path="/login">
-            <Login></Login>
-          </Route>
-          <Route path="/register">
-            <Register></Register>
-          </Route>
-          <Route path="*">
-            <NotFound></NotFound>
-          </Route>
-        </Switch>
-    </Router>
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/home">
+              <Home></Home>
+            </Route>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+            <Route path="/register">
+              <Register></Register>
+            </Route>
+            <Route path="*">
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -5,17 +5,26 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import logo from '../../../images/logo.png'
 import { Link } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
 
 const Navigation = () => {
+
+    const { user, logout } = useAuth();
+
     return (
         <div>
             <Box  sx={{ flexGrow: 1 }}>
                 <AppBar position="static" style={{backgroundColor: 'white'}}>
                     <Toolbar>
-                    <Link to="/"><img style={{width: 250, height: 100, marginLeft: "150px"}} src={logo} alt="" /></Link>
-                    <Box style={{marginLeft: "850px"}} >
-                        <Link to="/login" style={{color: '#1A354A', textDecoration: 'none'}} ><Button color="inherit">Login</Button></Link>
-                    </Box>
+                        <Link to="/"><img style={{width: 250, height: 100, marginLeft: "150px"}} src={logo} alt="" /></Link>
+                        <Box style={{marginLeft: "850px"}} >
+                            {
+                                user?.email ?
+                                <Button onClick={logout} color="inherit">Logout</Button>
+                                :
+                                <Link to="/login" style={{color: '#1A354A', textDecoration: 'none'}} ><Button color="inherit">Login</Button></Link>
+                            }
+                        </Box>
                     </Toolbar>
                 </AppBar>
             </Box>
