004c9753b6881c179da8a890e59ff13d2e499740
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-10 22:44:19 +0600

    vdve

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,10 +1,19 @@
+import { Container, Grid } from '@mui/material';
 import React from 'react';
+import login from '../../../images/login.svg'
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login</h2>
-        </div>
+        <Container>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Grid item xs={12} md={6}>
+                    
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <img style={{width: '100%', height: '600px'}} src={login} alt="" />
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
