fdb26204e4d48ee25e11f5eee50128beab1d6df8
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-11 21:38:26 +0600

    firvt

---
+++ src/Pages/Admin/AddProperties/AddProperties.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import axios from 'axios';
+import { useForm } from "react-hook-form";
+import './AddService.css';
+
+const AddProperties = () => {
+    
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        axios.post('https://mighty-hamlet-29284.herokuapp.com/services', data)
+            .then(res => {
+                if(res.data.insertedId){
+                    alert('added successfully');
+                    reset();
+                }
+            })
+    }
+
+    return (
+        <div className="add-service">
+            <h1 className="text-primary">Add a Service</h1>
+            <p>Discover your next great adventure, become an explorer to get started!</p>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("name", { required: true, maxLength: 100 })} placeholder="name" />
+                <textarea {...register("description")} placeholder="description" />
+                <input {...register("img")} placeholder="image url" />
+                <input className="btn btn-success" type="submit" />
+            </form>
+        </div>
+    );
+};
+
+export default AddProperties;
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import initializeAuthentication from '../Pages/Login/Firebase/firebase.initialize';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, updateProfile, signOut } from "firebase/auth";
 
 // initialize firebase app
 initializeAuthentication();
@@ -13,11 +13,20 @@ const useFirebase = () => {
 
     const auth = getAuth();
 
-    const registerUser = (email, password) => {
+    const registerUser = (email, password, name, history) => {
         setIsLoading(true);
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 setAuthError('');
+                const newUser = { email, displayName: name };
+                setUser(newUser);
+                // send name to firebase after creation
+                updateProfile(auth.currentUser, {
+                    displayName: name
+                }).then(() => {
+                }).catch((error) => {
+                });
+                history.replace('/');
             })
             .catch((error) => {
                 setAuthError(error.message);
