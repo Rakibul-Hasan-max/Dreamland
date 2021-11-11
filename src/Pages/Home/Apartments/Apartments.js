3da62dc0ea61f46591c06271628162c97592c87b
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-11 19:14:33 +0600

    firvbht

---
+++ src/Pages/Home/Apartments/Apartments.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Container, Typography } from '@mui/material';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const Properties = () => {
+    return (
+        <Container>
+            <Box sx={{ flexGrow: 1, mt: 6 }} >
+                <Typography  style={{fontSize: '30px', fontWeight: '500'}} variant="h3" component="div" gutterBottom>
+                    Latest Properties
+                </Typography>
+                <Typography style={{color: 'gray', marginBottom: '50px'}} variant="body1" gutterBottom>
+                    Our distinctive buildings fill the skyline and streetscapes of the city
+                </Typography>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                    {Array.from(Array(6)).map((_, index) => (
+                    <Grid item xs={2} sm={4} md={4} key={index}>
+                        <Item>xs=2</Item>
+                    </Grid>
+                    ))}
+                </Grid>
+            </Box>
+        </Container>
+    );
+};
+
+export default Properties;
+
+{/* <Typography variant="h6" component="div" gutterBottom>
+                                Luxury Villa in Richmond
+                            </Typography> */}
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Navigation from '../../Shared/Navigation/Navigation';
 import About from '../About/About';
+import Apartments from '../Apartments/Apartments';
 import Banner from '../Banner/Banner';
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
             <Navigation></Navigation>
             <Banner></Banner>
             <About></About>
+            <Apartments></Apartments>
             <Footer></Footer>
         </div>
     );
