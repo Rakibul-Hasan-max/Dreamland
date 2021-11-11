ff12eb921c9e31db5b5fad7c78ea1fc5eda95fa2
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-12 01:13:43 +0600

    good nt

--- src/Pages/Admin/AddProperties/AddProperties.js
+++ src/Pages/Admin/AddProperties/AddProperties.js
@@ -1,46 +1,21 @@
-// import React from 'react';
-// import axios from 'axios';
-// import { useForm } from "react-hook-form";
-// import './AddService.css';
-
-// const AddProperties = () => {
-    
-//     const { register, handleSubmit, reset } = useForm();
-//     const onSubmit = data => {
-//         console.log(data);
-//         axios.post('https://mighty-hamlet-29284.herokuapp.com/services', data)
-//             .then(res => {
-//                 if(res.data.insertedId){
-//                     alert('added successfully');
-//                     reset();
-//                 }
-//             })
-//     }
-
-//     return (
-//         <div className="add-service">
-//             <h1 className="text-primary">Add a Service</h1>
-//             <p>Discover your next great adventure, become an explorer to get started!</p>
-//             <form onSubmit={handleSubmit(onSubmit)}>
-//                 <input {...register("name", { required: true, maxLength: 100 })} placeholder="name" />
-//                 <textarea {...register("description")} placeholder="description" />
-//                 <input {...register("img")} placeholder="image url" />
-//                 <input className="btn btn-success" type="submit" />
-//             </form>
-//         </div>
-//     );
-// };
-
-// export default AddProperties;
-
+import axios from 'axios';
 import React from 'react';
 import { useForm } from "react-hook-form";
 import './AddProperties.css';
 
 const AddProperties = () => {
 
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        axios.post('http://localhost:5000/properties', data)
+            .then(res => {
+                if(res.data.insertedId){
+                    alert('added successfully');
+                    reset();
+                }
+            })
+    }
 
     return (
         <div className="form-style">
@@ -49,6 +24,7 @@ const AddProperties = () => {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <input {...register("name", { required: true, maxLength: 100 })} placeholder="name" />
                 <textarea {...register("description")} placeholder="description" />
+                <input type="number" {...register("price")} placeholder="price" />
                 <input {...register("img")} placeholder="image url" />
                 <input type="submit" />
             </form>
