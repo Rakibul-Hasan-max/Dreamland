cbdc4eb5b313ece53db8675cb173795dbd1b5e32
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-12 15:25:11 +0600

    goodt

--- src/Pages/Home/Apartments/Apartments.js
+++ src/Pages/Home/Apartments/Apartments.js
@@ -36,6 +36,43 @@ const Properties = () => {
 
 export default Properties;
 
-{/* <Typography variant="h6" component="div" gutterBottom>
-                                Luxury Villa in Richmond
-                            </Typography> */}
+
+
+
+/*
+import React, { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
+import Service from '../Service/Service';
+import './Services.css';
+
+const Services = () => {
+
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        fetch('https://mighty-hamlet-29284.herokuapp.com/services')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
+
+    return (
+        <div id="services">
+            <h2 className="text-primary mt-5">Our Services</h2>
+            <p className="text-success mb-5">“Adventure is worthwhile”</p>
+            {services.length===0 ? <Spinner animation="border" /> : 
+                <div className="service-container  container">
+                    {
+                        services.map(service => <Service
+                            key={service._id}
+                            service={service}
+                        ></Service>)
+                    }
+                </div>
+            }
+        </div>
+    );
+};
+
+export default Services;
+
+*/
