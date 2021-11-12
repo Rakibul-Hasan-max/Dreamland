195b2177c2a193945538ff40e9107cbf4c4e9fca
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-12 22:29:12 +0600

    some done

--- src/Pages/Properties/Purchase/Purchase.js
+++ src/Pages/Properties/Purchase/Purchase.js
@@ -1,7 +1,7 @@
 import { Box, Container } from '@mui/material';
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { useParams } from 'react-router';
 import useAuth from '../../../hooks/useAuth';
 import Footer from '../../Shared/Footer/Footer';
 import Navigation from '../../Shared/Navigation/Navigation';
@@ -9,24 +9,25 @@ import './Purchase.css';
 
 const Purchase = () => {
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
     const {user} = useAuth();
-    const onSubmit = data => {console.log(data)};
-
-    const { propertyId } = useParams();
-    const [property, setProperty] = useState({});
-
-    useEffect(() => {
-        fetch(`http://localhost:5000/properties`)
-            .then(res => res.json())
-            .then(data => setProperty(data));
-    }, [])
+    
+    const onSubmit = data => {
+        console.log(data);
+        axios.post('http://localhost:5000/purchase', data)
+            .then(res => {
+                if(res.data.insertedId){
+                    alert('added successfully');
+                    reset();
+                }
+            })
+        }
 
     return (
         <Box>
             <Navigation></Navigation>
             <Container>
-                <h2>Details of the properties and purchase {propertyId}</h2>
+                <h2>Details of the properties and purchase</h2>
                 <form className="purchase-form" onSubmit={handleSubmit(onSubmit)}>
                     <input placeholder="Name" defaultValue={user.displayName} {...register("name")} />                 <input placeholder="Email" defaultValue={user.email} {...register("email", { required: true })} />
                     {errors.email && <span className="error">This field is required</span>}
