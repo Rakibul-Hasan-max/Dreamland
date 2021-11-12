9c0f384507c781b7248c4385b6049cb260ad64ca
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-12 17:30:29 +0600

    goot

--- src/Pages/Properties/Property/Property.js
+++ src/Pages/Properties/Property/Property.js
@@ -9,7 +9,7 @@ const Property = ({ property }) => {
             <h3 className="mt-3">{name}</h3>
             <h3 className="mt-3">{price}</h3>
             <p className="px-3">{description}</p>
-            <Link to={`/property/${_id}`}>
+            <Link to={`/purchase/${_id}`}>
                 <button className="btn btn-warning">Book Now</button>
             </Link>
         </div>
