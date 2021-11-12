42f7c9546e346ba2902d3bd471c24e572b62a708
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-12 21:09:24 +0600

    ffffft

--- src/Pages/Properties/Purchase/Purchase.js
+++ src/Pages/Properties/Purchase/Purchase.js
@@ -1,7 +1,11 @@
+import { Box, Container } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router';
 import useAuth from '../../../hooks/useAuth';
+import Footer from '../../Shared/Footer/Footer';
+import Navigation from '../../Shared/Navigation/Navigation';
+import './Purchase.css';
 
 const Purchase = () => {
 
@@ -9,28 +13,31 @@ const Purchase = () => {
     const {user} = useAuth();
     const onSubmit = data => {console.log(data)};
 
-    const { serviceId } = useParams();
-    const [service, setService] = useState({});
+    const { propertyId } = useParams();
+    const [property, setProperty] = useState({});
 
     useEffect(() => {
         fetch(`http://localhost:5000/properties`)
             .then(res => res.json())
-            .then(data => setService(data));
+            .then(data => setProperty(data));
     }, [])
 
     return (
-        <div>
-            <h2>Details of the properties</h2>
-            <h2>This is booking: {serviceId}</h2>
-            <form className="booking-form" onSubmit={handleSubmit(onSubmit)}>
-                 <input placeholder="Name" defaultValue={user.displayName} {...register("name")} />                 <input placeholder="Email" defaultValue={user.email} {...register("email", { required: true })} />
-                 {errors.email && <span className="error">This field is required</span>}
-                 <input placeholder="Phone number" defaultValue="" {...register("phone")} />
-                 <input placeholder="Address" defaultValue="" {...register("address")} />
-                <input placeholder="Zip code" defaultValue="" {...register("zip code")} />
-                <input className="submit" type="submit" />
-             </form>
-        </div>
+        <Box>
+            <Navigation></Navigation>
+            <Container>
+                <h2>Details of the properties and purchase {propertyId}</h2>
+                <form className="purchase-form" onSubmit={handleSubmit(onSubmit)}>
+                    <input placeholder="Name" defaultValue={user.displayName} {...register("name")} />                 <input placeholder="Email" defaultValue={user.email} {...register("email", { required: true })} />
+                    {errors.email && <span className="error">This field is required</span>}
+                    <input placeholder="Phone number" defaultValue="" {...register("phone")} />
+                    <input placeholder="Address" defaultValue="" {...register("address")} />
+                    <input placeholder="Zip code" defaultValue="" {...register("zip code")} />
+                    <input className="submit" type="submit" />
+                </form>
+            </Container>
+            <Footer></Footer>
+        </Box>
     );
 };
 
