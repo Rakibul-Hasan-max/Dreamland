4a242138aca35d252566a16023ef0a67cbbc3a0c
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-13 21:22:37 +0600

    admin

--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -12,22 +12,18 @@ const MakeAdmin = () => {
         setEmail(e.target.value);
     }
     const handleAdminSubmit = e => {
-        // const user = { email };
-        // fetch('http://localhost:5000/users/admin', {
-        //     method: 'PUT',
-        //     headers: {
-        //         'authorization': `Bearer ${token}`,
-        //         'content-type': 'application/json'
-        //     },
-        //     body: JSON.stringify(user)
-        // })
-        //     .then(res => res.json())
-        //     .then(data => {
-        //         if (data.modifiedCount) {
-        //             console.log(data);
-        //             setSuccess(true);
-        //         }
-        //     })
+        const user = { email };
+        fetch('http://localhost:5000/users/admin', {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
 
         e.preventDefault()
     }
