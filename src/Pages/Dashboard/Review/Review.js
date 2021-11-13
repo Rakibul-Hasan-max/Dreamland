d5af8f012cf4d7116ec28bcfa85ba0f6dca28d1f
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-13 22:45:35 +0600

    admhge

--- src/Pages/Dashboard/Review/Review.js
+++ src/Pages/Dashboard/Review/Review.js
@@ -1,9 +1,29 @@
+import axios from 'axios';
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const Review = () => {
+
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        axios.post('http://localhost:5000/review', data)
+            .then(res => {
+                if(res.data.insertedId){
+                    alert('review submitted');
+                    reset();
+                }
+            })
+    }
+
     return (
-        <div>
-            <h2>this is review page</h2>
+        <div className="form-style">
+            <h1>Give a review</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("name", { required: true, maxLength: 100 })} placeholder="Your name" />
+                <textarea {...register("description")} placeholder="Your review" />
+                <input type="submit" />
+            </form>
         </div>
     );
 };
